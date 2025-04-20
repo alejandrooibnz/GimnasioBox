c90a90afad48eefecfb825c82da114df0a32ecb2
HugoGali1 <hugo.galianareal@cieep.com>
2025-04-20 21:35:08 +0200

    f

---
+++ src/app/components/services/producto.service.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
+import { Producto } from '../interfaces/producto';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html'
+})
+export class ProductoComponent implements OnInit {
+
+  producto!: Producto;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productoService: ProductoService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productoService.getProductoPorId(id).subscribe(producto => {
+      this.producto = producto;
+    });
+  }
+
+}
+getProductoPorId(id: number): Observable<Producto> {
+    return this.getProductos().pipe(
+      map(productos => productos.find(p => p.id === id)!)
+    );
+  }
+  import { map } from 'rxjs/operators';
